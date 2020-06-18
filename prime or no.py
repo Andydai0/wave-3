f8f1bed8d3aaeb53e18312e358a055218a6bd74f
Andydai0 <63471304+Andydai0@users.noreply.github.com>
2020-06-18 16:29:01 -0400

    Add files via upload

---
+++ prime or no.py
@@ -0,0 +1,11 @@
+n = int(input("Please input an integer larger than 1: "))
+print("This number is a Prime?")
+if n <= 1:
+    print("Error, please insert valid value")
+else:
+    for i in range(2, n):
+        if (n % i) == 0:
+            print("False")
+            break
+    else:
+            print("True")
